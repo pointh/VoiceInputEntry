19622c4ec6b67a64925e9dee427f09b8b7a51e3e
Pavel Svec <pavel.svec@itcrawl.cz>
2022-02-01 09:40:01 +0100

    Add project files.

---
+++ VoiceInputEntry/VoiceInputEntry/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Diagnostics;
+
+namespace VoiceInputEntry
+{
+    public partial class MainPage : ContentPage
+    {
+        public Command SpeakCommand { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            SpeakCommand = new Command(async () =>
+            {
+                var locales = await TextToSpeech.GetLocalesAsync();
+                var settings = new SpeechOptions()
+                {
+                    Volume = 1f,
+                    Pitch = 1f,
+                    Locale = locales.FirstOrDefault(t => t.Language == "cs")
+                };
+                if (string.IsNullOrEmpty(TextEntry.Text) == false)
+                await TextToSpeech.SpeakAsync(TextEntry.Text, settings);
+            });
+            BindingContext = this;
+        }
+    }
+}
